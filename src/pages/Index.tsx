2edc689f6ee725bdec184267e52b2f7bcfed85d1
Bhaskar3292 <150072968+Bhaskar3292@users.noreply.github.com>
2026-03-16 15:14:45 -0400

    Remove booking form from home page

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -2,7 +2,6 @@ import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero";
 import ServiceGrid from "@/components/ServiceGrid";
 import ComplianceBar from "@/components/ComplianceBar";
-import BookingForm from "@/components/BookingForm";
 import OfferCoverageSection from "@/components/OfferCoverageSection";
 import Footer from "@/components/Footer";
 
@@ -16,7 +15,6 @@ const Index = () => {
         <div id="services">
           <ServiceGrid />
         </div>
-        <BookingForm />
         <OfferCoverageSection />
       </main>
       <Footer />
